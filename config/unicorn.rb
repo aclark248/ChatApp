f2a631ae0592e6e9096b347d6a729c5616875723
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2021-08-21 13:50:04 -0400

    Update unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,24 @@
-# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/srv/ChatApp"
 
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/var/www/my_app/pids/unicorn.pid"
 
-# Set unicorn options
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/my_app/log/unicorn.log"
+stdout_path "/var/www/my_app/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.myapp.sock"
+
+# Number of processes
+# worker_processes 4
 worker_processes 2
-preload_app true
+
+# Time-out
 timeout 30
-
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
